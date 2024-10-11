85058f82ba90343c898206991b1629e38c05c25a
luszczewskakasia <kasia.luszczewska13@gmail.com>
2024-10-11 13:49:29 +0200

    changed index.js to collaborate with socket.io

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-// Include the server in your file
-const server = require('server');
-const { get, post } = server.router;
-var http = require('http');
-var fs = require('fs');
 
-const PORT=8080; 
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
 
+const app = express();
+const server = createServer(app);
+const io = new Server(server)
 
-fs.readFile('index.html', function (err, html) {
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        // response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT,()=> {
-        console.log(`Serwer działa na porcie ${PORT}`);
-        console.log(`Skrypt uruchomiony w folderze: ${__dirname}`);
-    });
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
 });
 
-// http://localhost:8080/ <- nasza stronka 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
+
+server.listen(8080, () => {
+  console.log('server running at http://localhost:8080');
+});
