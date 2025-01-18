2a31909ec05d23c1be0198877a504ee31c7e56b4
luszczewskakasia <kasia.luszczewska13@gmail.com>
2025-01-18 14:57:59 +0100

    xd

--- public/main.js
+++ public/main.js
@@ -169,7 +169,7 @@ class ChessScene {
         // console.log(this.board);
 
         // Uruchom zegar z początkowym czasem 10 minut
-        hud.ClockAnimation(600, 600, this);
+        
 
     }
 
@@ -187,6 +187,9 @@ class ChessScene {
         });
         document.getElementById('White_resignButton').addEventListener('click', () => {
             hud.endGame('black');
+        });
+        document.getElementById('Black_startButton').addEventListener('click', () => {
+            hud.ClockAnimation(600, 600, this);
         });
     }
 
@@ -222,6 +225,8 @@ class ChessScene {
                     this.turn = !this.turn;
                     this.draggable_obj.userData.row = row
                     this.draggable_obj.userData.column = col
+
+                    sendMove({ fromRow, fromCol, toRow: row, toCol: col });
                 }
 
                 if(this.fieldArray[row][col].piece != null && this.fieldArray[row][col].piece.color !== this.draggable_obj.userData.color)
